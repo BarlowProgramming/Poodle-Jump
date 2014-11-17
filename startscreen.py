3924d1d6d5e4f8f5773a4a61093e4f097facf3c6
jaywunder <jaywunder@gmail.com>
2014-11-17 11:28:21 -0500

    updated startscreen.py

--- startscreen.py
+++ startscreen.py
@@ -1,40 +1,83 @@
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics import Rectangle, Ellipse, Line
+
+NOTEXTURE = "assets/NOTEXTURE.png"
 
 class StartScreen(Widget):
     def __init__(self):
         super(StartScreen, self).__init__()
         self.layout = FloatLayout()
         self.add_widget(self.layout)
-        self.startText = Label(text="Poodle Jump 2014 Edition Platinum Edition 2014",
-                               center_x=Window.width / 2,
-                               center_y=Window.height / 2,
-                               font_size=32)
+        #Creating Widgets
+        self.background = Image(
+            source=NOTEXTURE,
+            texture_size=Window.size,
+            size=Window.size,
+            allow_stretch=True,
+            keep_ratio=False,
+            x=0, y=0
+        )
+        self.logo = Image(
+            source=NOTEXTURE,
+            texture_size=(300, 300),
+            size=(300, 300),
+            center_x=Window.width / 2,
+            center_y=(Window.height / 2) + 150
+        )
+        self.startText = Label(
+            text="Poodle Jump 2014 Edition Platinum Edition #2014",
+            center_x=Window.width / 2,
+            center_y=Window.height / 2,
+            font_size=32
+        )
+        self.startButton = Button(
+            text="Start Game",
+            width=300,
+            height=75,
+            center_x=Window.width / 2,
+            center_y=Window.height / 2 - 100,
+            font_size=32
+        )
+        self.settingsButton = Button(
+            text="Settings",
+            width=300,
+            height=75,
+            center_x=Window.width / 2,
+            center_y=Window.height / 2 - 200,
+            font_size=32
+        )
+        self.mouseTexture = Image(
+            source=NOTEXTURE,
+            texture_size=(20, 20),
+            size=(20, 20)
+        )
+        #Adding Widgets
+        self.layout.add_widget(self.background)
+        self.layout.add_widget(self.logo)
         self.layout.add_widget(self.startText)
+        self.layout.add_widget(self.startButton)
+        self.layout.add_widget(self.settingsButton)
+        self.layout.add_widget(self.mouseTexture)
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
-        self.startText.bind(on_touch_move=self.on_touch_move)
 
     def on_touch_move(self, *args):
-        self.startText.center_x = Window.mouse_pos[0]
-        self.startText.center_y = Window.mouse_pos[1]
+        pass
 
     def update(self, *args):
-        self.canvas.after.clear()
-        self.startText.canvas.after.clear()
-        with self.startText.canvas.after:
-            Rectangle(pos=(self.startText.x,
-                           self.startText.y),
-                      size=(self.startText.width,
-                            self.startText.height))
+        self.mouseTexture.center_x = Window.mouse_pos[0]
+        self.mouseTexture.center_y = Window.mouse_pos[1]
 
-        with self.canvas.after:
-            Rectangle(pos=(Window.mouse_pos[0],
-                           Window.mouse_pos[1]),
-                      size=(10,
-                            10))
+        # self.canvas.after.clear()
+        # self.startText.canvas.after.clear()
+        # with self.canvas.after:
+        #     Rectangle(pos=(Window.mouse_pos[0],
+        #                    Window.mouse_pos[1]),
+        #               size=(10,
+        #                     10))
