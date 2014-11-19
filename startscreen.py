04ef991e9b6d9930904c8d0656dcdb9ab8371056
jaywunder <jaywunder@gmail.com>
2014-11-18 21:01:21 -0500

    Fixed the start screen, sorry Henry

--- startscreen.py
+++ startscreen.py
@@ -2,17 +2,13 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics import Rectangle, Ellipse, Line
 
-
-#Commit for the hell of it
-
 NOTEXTURE = "assets/NOTEXTURE.png"
-CURSOR = "assets/CURSOR.png"
+
 
 class StartScreen(Widget):
     def __init__(self):
@@ -21,60 +17,68 @@ class StartScreen(Widget):
         self.add_widget(self.layout)
         #Creating Widgets
         self.background = Image(
-            width=Window.width,
-            height=Window.height,
-            color=(255, 0, 0, 1)
+            source=NOTEXTURE,
+            texture_size=Window.size,
+            size=Window.size,
+            allow_stretch=True,
+            keep_ratio=False,
+            x=0, y=0
+        )
+        self.logo = Image(
+            source=NOTEXTURE,
+            texture_size=(300, 300),
+            size=(300, 300),
+            center_x=Window.width / 2,
+            center_y=(Window.height / 2) + 150
         )
         self.startText = Label(
-            text="Poodle Jump",
-            width=300,
-            height=75,
-            center_x=Window.width / 2,
-            center_y=Window.height / 2 + 100,
-            font_size=50
-        )
-        self.startButton = Button(
-            text="Start",
-            width=300,
-            height=75,
+            text="Poodle Jump 2014 Edition Platinum Edition #2014",
             center_x=Window.width / 2,
             center_y=Window.height / 2,
             font_size=32
         )
-        self.settingsButton = Button(
-            color=(0, 0, 0),
-            text="Settings",
-            width=300,
-            height=75,
+        self.startButton = Button(
+            text="Start Game",
+            width=Window.width / 2.6,
+            height=Window.height / 8,
             center_x=Window.width / 2,
             center_y=Window.height / 2 - 100,
             font_size=32
         )
+        self.settingsButton = Button(
+            text="Settings",
+            width=Window.width / 2.6,
+            height=Window.height / 8,
+            center_x=Window.width / 2,
+            center_y=Window.height / 2 - 200,
+            font_size=32
+        )
+
+        #matt did this button all by himself 11/18/14 python experiences id put a hashtag but thats meant for comments
+        #dont tell me what to do jacob
+        self.optionsButton = Image(
+            source=NOTEXTURE,
+            texture_size=(40, 40),
+            size=(40, 40),
+            center_x=Window.width - 40,
+            center_y=Window.height - Window.height + 40
+        )
         self.mouseTexture = Image(
-            source=CURSOR,
+            source=NOTEXTURE,
             texture_size=(20, 20),
             size=(20, 20)
         )
         #Adding Widgets
         self.layout.add_widget(self.background)
+        self.layout.add_widget(self.logo)
         self.layout.add_widget(self.startText)
         self.layout.add_widget(self.startButton)
         self.layout.add_widget(self.settingsButton)
+        self.layout.add_widget(self.optionsButton)
         self.layout.add_widget(self.mouseTexture)
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
-    def on_touch_move(self, *args):
-        pass
-
     def update(self, *args):
-        self.mouseTexture.center_x = Window.mouse_pos[0]
-        self.mouseTexture.center_y = Window.mouse_pos[1]
-
-        # self.canvas.after.clear()
-        # self.startText.canvas.after.clear()
-        # with self.canvas.after:
-        #     Rectangle(pos=(Window.mouse_pos[0],
-        #                    Window.mouse_pos[1]),
-        #               size=(10,
-        #                     10))
+        self.mouseTexture.x = Window.mouse_pos[0]
+        self.mouseTexture.y = Window.mouse_pos[1] - self.mouseTexture.height
